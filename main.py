fea06f31b9e5695ff3de29a54aec2c31c476e133
youngnu <qyahzn2004@gmail.com>
2023-12-13 00:18:04 +0900

    browser.page_source

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ browser = webdriver.Chrome() #import webdriver -> browser = webdriver.Chrome() -
 base_url="https://kr.indeed.com/jobs?q="
 search_term="python"
 browser.get(f"{base_url}{search_term}")
+print(browser.page_source) #requests의 get -> response = get("URL") => response.text의 기능을 수행
 
 while (True): #창이 잠깐 켜졌다 꺼지는 오류를 해결 
     pass
