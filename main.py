222f8b792fb83a0d0a44da60ffb1cf97811e768f
youngnu <qyahzn2004@gmail.com>
2023-12-12 21:44:36 +0900

    beautifulsoup

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from requests import get
+from bs4 import BeautifulSoup
 
+base_url="https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
+search_term="python"
+
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+    print("Nooooo!!!")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all("section", class_="jobs")
+    print(len(jobs))
