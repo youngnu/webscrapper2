0f29b62ed4d08729643322383a8f0b97f052569a
youngnu <qyahzn2004@gmail.com>
2023-12-13 00:14:23 +0900

    selenium default setting

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from extractors.wwr import extract_wwr
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-jobs = input("What find job?")
-print(extract_wwr(jobs))
+browser = webdriver.Chrome() #import webdriver -> browser = webdriver.Chrome() -> browser.get("URL")
+base_url="https://kr.indeed.com/jobs?q="
+search_term="python"
+browser.get(f"{base_url}{search_term}")
+
+while (True): #창이 잠깐 켜졌다 꺼지는 오류를 해결 
+    pass
