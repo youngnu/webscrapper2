7862ed03608f580f4c667f4dfc08bcb58455ef7f
youngnu <qyahzn2004@gmail.com>
2023-12-16 15:54:43 +0900

    make fake database

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from extractors.indeed import extract_indeed
 from extractors.wwr import extract_wwr
 from extractors.wanted import extract_wanted
 
 
 app = Flask('JobScrapper')
+
+db={}
 
 @app.route("/")
 def home():
@@ -13,10 +15,16 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    indeed = extract_indeed(keyword)
-    wwr = extract_wwr(keyword)
-    wanted = extract_wanted(keyword)
-    jobs  = indeed + wwr + wanted
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed(keyword)
+        wwr = extract_wwr(keyword)
+        wanted = extract_wanted(keyword)
+        jobs  = indeed + wwr + wanted
+        db[keyword] = jobs
     return render_template("seacrh.html", keyword=keyword, jobs = jobs)
 
 app.run(debug=True)
